826b8d79108fd2b9a14b95a4e3dda7736a3a432a
ND005 <nimmaladinesh5@gmail.com>
2025-11-08 18:55:34 +0530

    11/08/2025 : Automation project created with basic files & dependencies

---
+++ src/main/java/web_ui_testing/automation_suite/App.java
@@ -0,0 +1,13 @@
+package web_ui_testing.automation_suite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
