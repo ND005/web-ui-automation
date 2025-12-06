6a10a123807e56698253b2ff87c8b7ae70a53744
ND005 <nimmaladinesh5@gmail.com>
2025-12-06 22:45:44 +0530

    12/06/2025 : Jenkins connection

--- src/test/java/com/webui/runner/TestRunner.java
+++ src/test/java/com/webui/runner/TestRunner.java
@@ -6,8 +6,9 @@ import io.cucumber.junit.CucumberOptions;
 
 //@SuppressWarnings("deprecation")
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/Test-Feature-Files", glue = {"com.webui.stepDefinitions"}, plugin = {
-		"pretty", "html:target/cucumber-reports.html" })
+@CucumberOptions(features = "src/test/resources/Test-Feature-Files", glue = { "com.webui.stepDefinitions" }, plugin = {
+		"pretty", "html:target/cucumber-html-report", "json:target/cucumber.json",
+		"junit:target/surefire-reports/cucumber.xml" })
 public class TestRunner {
 
 }
